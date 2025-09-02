90b39234cb966e1b4f37ab9b2c5c469dc196f125
Santhoshkumar <santhosh020316@gmail.com>
2025-09-03 00:09:44 +0530

    fix: build error

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  redirects: async () => [
-    {
-      source: '/:path*',
-      has: [{ type: 'host', value: 'https://www.consoledotlog.in/' }],
-      destination: 'https://consoledotlog.in/:path*',
-      permanent: true,
-    },
-  ],
 };
 
 module.exports = nextConfig;
