1280c09c92bfa2b0c0f06a72afcd277dccbb0ea8
Santhoshkumar <santhosh020316@gmail.com>
2025-09-03 00:07:48 +0530

    Initial commit: Portfolio project setup

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  redirects: async () => [
+    {
+      source: '/:path*',
+      has: [{ type: 'host', value: 'https://www.consoledotlog.in/' }],
+      destination: 'https://consoledotlog.in/:path*',
+      permanent: true,
+    },
+  ],
 };
 
 module.exports = nextConfig;
